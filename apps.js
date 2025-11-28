3c5896d7cc4b0615e134b0eb4fd92efa6c724ce6
Nene Nnoruka <237324102+clarioncbbii@users.noreply.github.com>
2025-11-28 15:54:19 +0000

    "feat: "image thumbnail bar almost done"

--- apps.js
+++ apps.js
@@ -18,6 +18,34 @@ const imageData = [
     imageSrc: "./assets/images/waterfall-nigeria.webp",
     imageAlt: "Waterfall in the middle of the forest",
   },
+  {
+    imageName: "PHOTO 4",
+    imageSrc: "./assets/images/beach.webp",
+    imageAlt:
+      "Sandy beack in Lagos, Nigeria; people swimming in the ocean in the distance",
+  },
+  {
+    imageName: "PHOTO 5",
+    imageSrc: "./assets/images/fruit.webp",
+    imageAlt:
+      "Fruit basket in the market containing Avocado, Agbalumo (African Star Apple; Pineapple in the background",
+  },
+  {
+    imageName: "PHOTO 6",
+    imageSrc: "./assets/images/sunset.webp",
+    imageAlt: "A sandy beach with waves crashing; Sunset at Elegushi beach",
+  },
+  {
+    imageName: "PHOTO 7",
+    imageSrc: "./assets/images/pepper.webp",
+    imageAlt: "Woman selling pepper watches over her wares in a market.",
+  },
+  {
+    imageName: "PHOTO 8",
+    imageSrc: "./assets/images/village.webp",
+    imageAlt:
+      "An aerial view of a village in the middle of a forest in Nigeria",
+  },
 ];
 
 console.log(imageData);
@@ -40,12 +68,14 @@ console.log(imageData);
 function createThumbnails() {
   const thumbnailContainer = document.getElementById("thumbnail-container");
   for (let i = 0; i < imageData.length; i++) {
-    const currentImageData = imageData[i]; //MOVE TO CREATE GLOBAL SCOPE
+    const currentImageData = imageData[i];
     const thumbnailImage = document.createElement("img");
     thumbnailImage.src = currentImageData.imageSrc;
     thumbnailImage.alt = currentImageData.imageAlt;
     thumbnailImage.className = "thumbnail-image";
-    //   thumbnailImage.addEventListener("click", function (event){createFullscreenImages}; ????
+    thumbnailImage.addEventListener("click", function () {
+      createFullscreenImages(currentImageData);
+    });
     thumbnailContainer.appendChild(thumbnailImage);
   }
 }
@@ -66,17 +96,34 @@ createThumbnails();
 //append the image to the container
 // }
 
-function createFullscreenImages() {
+function createFullscreenImages(currentImageData) {
   const fullscreenContainer = document.getElementById("fullscreen-container");
-  document.fullscreenContainer;
-  //delete image how?
   fullscreenContainer.innerHTML = null;
   const fullscreenImage = document.createElement("img");
-  fullscreenImageImage.src = currentImageData.imageSrc;
-  fullscreenImageImage.alt = currentImageData.imageAlt;
-  fullscreenImageImage.className = "fullscreen-image";
-  fullscreenContainer.appendChild(fullscreenImageImage);
+  fullscreenImage.src = currentImageData.imageSrc;
+  fullscreenImage.alt = currentImageData.imageAlt;
+  fullscreenImage.className = "fullscreen-image";
+  fullscreenContainer.appendChild(fullscreenImage);
 }
 
 //add the createFullscreenImages function as the event handler of the event above
 //call the createThumbnails function
+
+//TODO: Make the buttons clickable
+const prevBtn = document.getElementById("previous");
+const nextBtn = document.querySelector("next");
+let currentImageIndex = 0;
+
+// TODO: Next button
+// Need a function as addEventHandler
+// We want function to: when button clicked, +1 to currentImageIndex
+// We want to validate currentImageIndex against imageData.length-1
+// if false, adjust currentImageIndex to 0
+// create fullscreen images using imageData[currentImageIndex]
+
+//  nextBtn.addEventListener("click",
+//      #EVENTHANDLER FUNCTION);
+//    });
+
+//TODO: Previous button
+// Do opposite for previous button
